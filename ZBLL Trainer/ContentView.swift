c4cbf27d18256ab4ef375bf24947e3a6808b81c9
Noah Westendorf <noahwestendorfwo@gmail.com>
2023-06-22 18:15:34 +0200

    While the button is pressed for less than half a second, it's color is red and the timer can not be started.

--- ZBLL Trainer/ContentView.swift
+++ ZBLL Trainer/ContentView.swift
@@ -11,12 +11,14 @@ struct ContentView: View {
     
     @State var TimerCount = 0.0
     @State var TimerIsRunning = false
-    @State var currentColor = Color(uiColor: .white)
+    @State var HeldDownTimerCount = 0.0
+    @State var HeldDownTimerIsRunnning = false
+    @State var CurrentColor = Color(uiColor: .white)
     @State private var canTouchDown = true
     @State var TimerWasRunning = false
     
     let timer = Timer.publish(every: 0.01, on: .main, in: .common) .autoconnect()
-    
+    let HeldDownTimer = Timer.publish(every: 0.01, on: .main, in: .common) .autoconnect()
     
     
     var body: some View {
@@ -29,35 +31,59 @@ struct ContentView: View {
                         DragGesture(minimumDistance:0)
                             .onChanged{ value in
                                 if canTouchDown{
+                                    
+                                    
+                                    
                                     if (TimerIsRunning == true){
+                                        
                                         TimerWasRunning = true
                                         TimerIsRunning = false
                                         
                                     } else if(TimerIsRunning == false) {
+                                        HeldDownTimerIsRunnning = true
                                         TimerWasRunning = false
-                                        currentColor = Color(uiColor: .green)
-                                        TimerCount = 4
+                                        TimerCount = 0
                                     }
                                 }
                                 canTouchDown = false
+                                
                             }
                             .onEnded{ Value in
-                                if (TimerWasRunning == false){
+                                if (TimerWasRunning == false && CurrentColor == Color(uiColor: .green)){
                                     TimerIsRunning = true
                                 } else {
                                     TimerIsRunning = false
                                 }
+                                HeldDownTimerCount = 0
+                                HeldDownTimerIsRunnning = false
+                                CurrentColor = Color(uiColor: .white)
                                 canTouchDown = true
-                                currentColor = Color(uiColor: .white)
                             }
                         )
                 VStack{
-                    Text("R2 F' B' L2 F' B D' L2 F2 U L2 U' R2 U L2 B2") //future feature:tap on scramble and get description of the previous solves with it
+                    Text("R2 F' B' L2 F' B D' L2 F2 U L2 U' R2 U L2 B2") //future feature: tap on scramble and get description of the previous solves with it
                         .fontWeight(.bold)
                         .font(.title)
                         .padding(.vertical, 40.0)
                     
                     Spacer()
+                    
+                        .onReceive(HeldDownTimer) { _ in
+                            if (HeldDownTimerIsRunnning) {
+                                
+                                HeldDownTimerCount += 0.01
+                                
+                                if (HeldDownTimerCount <= 0.5) {
+                                    CurrentColor = Color(uiColor: .red)
+                                } else {
+                                    CurrentColor = Color(uiColor: .green)
+                                }
+                            } else {
+                                
+                                HeldDownTimerIsRunnning = false
+                            }
+                            
+                        }
                     
                     Text(String(format: "%.2f", TimerCount))
                         .onReceive(timer) { _ in
@@ -71,7 +97,7 @@ struct ContentView: View {
                             }
                         }
                         .font(.system(size: 50, weight: .bold))
-                        .foregroundColor(currentColor)
+                        .foregroundColor(CurrentColor)
                     
                     
                     Spacer()
